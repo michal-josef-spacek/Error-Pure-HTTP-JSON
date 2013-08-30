bba7b1ce222bb05d37ee9662038e42003c79bb26
Michal Špaček <tupinek@gmail.com>
2013-08-30 16:43:01 +0200

    Prepare for 0.02 version.

--- JSON.pm
+++ JSON.pm
@@ -16,7 +16,7 @@ Readonly::Array our @EXPORT_OK => qw(err);
 Readonly::Scalar my $EVAL => 'eval {...}';
 
 # Version.
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 # Ignore die signal.
 $SIG{__DIE__} = 'IGNORE';
@@ -190,6 +190,6 @@ BSD license.
 
 =head1 VERSION
 
-0.01
+0.02
 
 =cut
