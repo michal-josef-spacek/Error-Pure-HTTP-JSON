47c4840b2a3b792cf41813eb28f81b4eb7ff01f6
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-11-24 16:16:48 +0100

    Update author name

--- JSON.pm
+++ JSON.pm
@@ -165,13 +165,13 @@ L<https://github.com/tupinek/Error-Pure-HTTP-JSON>
 
 =head1 AUTHOR
 
-Michal Špaček L<mailto:skim@cpan.org>
+Michal Josef Špaček L<mailto:skim@cpan.org>
 
 L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2013-2015 Michal Špaček
+ © 2013-2015 Michal Josef Špaček
  BSD 2-Clause License
 
 =head1 VERSION
--- JSON/Advance.pm
+++ JSON/Advance.pm
@@ -199,13 +199,13 @@ L<https://github.com/tupinek/Error-Pure-JSON>
 
 =head1 AUTHOR
 
-Michal Špaček L<mailto:skim@cpan.org>
+Michal Josef Špaček L<mailto:skim@cpan.org>
 
 L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2013-2015 Michal Špaček
+ © 2013-2015 Michal Josef Špaček
  BSD 2-Clause License
 
 =head1 VERSION
